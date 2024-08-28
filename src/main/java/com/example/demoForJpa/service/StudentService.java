75199b479de79e7e0a95754be5d9b22b006adf76
Gowtham <Sai.Gowtham@zaggle.in>
2024-08-28 15:03:02 +0530

    second commit

--- src/main/java/com/example/demoForJpa/service/StudentService.java
+++ src/main/java/com/example/demoForJpa/service/StudentService.java
@@ -8,7 +8,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.List;
